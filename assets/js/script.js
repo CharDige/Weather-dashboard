49488a2f5ea027f8d9dda70489cfbc539f5606a0
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-04-16 18:24:14 +0930

    Changed jQuery to JavaScript for preference. Added displayCurrentCityWeather function, appending searched city name and current date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
-var citySearch = $("#city-search");
-var citySearchInputEl = $("#city-search-input");
-var searchButton = $("#search-button");
-var errorMessageEl = $("#error-message");
-var searchedCitiesEl = $("#searched-cities");
-var todayWeather = $("#today-weather-card");
-var fiveDayForecast = $("#five-day-forecast");
+var citySearch = document.querySelector("#city-search");
+var citySearchInputEl = document.querySelector("#city-search-input");
+var searchButton = document.querySelector("#search-button");
+var errorMessageEl = document.querySelector("#error-message");
+var searchedCitiesEl = document.querySelector("#searched-cities");
+var todayWeather = document.querySelector("#today-weather-card");
+var fiveDayForecast = document.querySelector("#five-day-forecast");
 
 
 var ApiKey = "2eafffb05ee0e15aaa7a1382d485fad9";
@@ -12,22 +12,35 @@ var ApiKey = "2eafffb05ee0e15aaa7a1382d485fad9";
 // Search button function
 var searchButtonHandler = function(event) {
     event.preventDefault();
-    var searchedCity = citySearchInputEl.val();
+    var searchedCity = citySearchInputEl.value.trim();
     console.log(searchedCity);
-    console.log(getCurrentCityWeather());
+    console.log(getCurrentCityWeather(searchedCity));
 }
 
 // Get current weather for searched city
-var getCurrentCityWeather = function(city) {
-    var searchedCity = citySearchInputEl.val();
+var getCurrentCityWeather = function(searchedCity) {
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&units=imperial&appid=" + ApiKey
     fetch(apiUrl)
     .then (function(response) {
         response.json()
         .then(function(data) {
-            console.log(data, city);
+            displayCurrentCityWeather(data, searchedCity);
         })
     })
 };
 
-searchButton.on("click", searchButtonHandler);
+// Display searched city's current weather
+var displayCurrentCityWeather = function(weather, searchedCity) {
+    // Clear content ahead of new content
+    todayWeather.textContent = " ";
+    var todayWeatherHeading = document.createElement("h2");
+    todayWeatherHeading.textContent = searchedCity;
+    todayWeather.appendChild(todayWeatherHeading);
+
+    // Current date
+    var currentDate = document.createElement("p");
+    currentDate.textContent = moment().format("D MMM YYYY");
+    todayWeather.appendChild(currentDate);
+}
+
+searchButton.addEventListener("click", searchButtonHandler);
