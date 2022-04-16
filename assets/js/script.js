e53cee62cfbfc1e0c40c884f7a878c5afbbf4c56
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-04-16 17:56:48 +0930

    Added getCurrentCityWeather function, testing results to console

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,31 +2,32 @@ var citySearch = $("#city-search");
 var citySearchInputEl = $("#city-search-input");
 var searchButton = $("#search-button");
 var errorMessageEl = $("#error-message");
+var searchedCitiesEl = $("#searched-cities");
 var todayWeather = $("#today-weather-card");
 var fiveDayForecast = $("#five-day-forecast");
 
 
 var ApiKey = "2eafffb05ee0e15aaa7a1382d485fad9";
 
-var ApiUrl = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=" + ApiKey
-
-// Testing search button
+// Search button function
 var searchButtonHandler = function(event) {
     event.preventDefault();
-
-    var showResults = function() {
-        fetch(ApiUrl)
-        .then (function (response) {
-            if (response.ok) {
-                response.json()
-                .then(function (data) {
-                    console.log(data);
-                })
-            }
-        })
-    }
-
-    showResults();
+    var searchedCity = citySearchInputEl.val();
+    console.log(searchedCity);
+    console.log(getCurrentCityWeather());
 }
+
+// Get current weather for searched city
+var getCurrentCityWeather = function(city) {
+    var searchedCity = citySearchInputEl.val();
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&units=imperial&appid=" + ApiKey
+    fetch(apiUrl)
+    .then (function(response) {
+        response.json()
+        .then(function(data) {
+            console.log(data, city);
+        })
+    })
+};
 
 searchButton.on("click", searchButtonHandler);
